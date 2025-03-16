6a5d9f159e69d542095a927c5e4a2860300ef061
xntkrnl <maminbors@gmail.com>
2025-03-17 02:14:30 +0300

    TODO

---
+++ RandomTerrainGenerator/Patches/TODO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomTerrainGenerator.Patches
+{
+    internal class TODO
+    {
+        //roundmanager patch before ainodes generate
+        //make main/fireexit prefabs
+        //landscape scaner
+    }
+}
