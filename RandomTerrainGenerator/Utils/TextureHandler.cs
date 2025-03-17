c2595bdd59473ae46a76f15dff762c1136f70e5b
Nikita <zhuravlyov.neke@yandex.ru>
2025-03-17 22:58:30 +0300

    toRenderTexture()

--- RandomTerrainGenerator/Utils/TextureHandler.cs
+++ RandomTerrainGenerator/Utils/TextureHandler.cs
@@ -18,14 +18,28 @@ namespace RandomTerrainGenerator.Utils
             Plugin.Log($"HeightMap saved in: {pathAndFileName}");
         }
 
-        internal static Texture2D ToTexture2D(RenderTexture rTex, int resolution)
+        internal static Texture2D ToTexture2D(RenderTexture renderTexture, int resolution)
         {
-            Texture2D tex = new Texture2D(resolution, resolution, TextureFormat.RGB24, false);
+            RenderTexture currentActiveRT = RenderTexture.active;
+            Texture2D texture2D = new Texture2D(resolution, resolution, TextureFormat.RGB24, false);
 
-            RenderTexture.active = rTex;
-            tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
-            tex.Apply();
-            return tex;
+            RenderTexture.active = renderTexture;
+            texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            texture2D.Apply();
+
+            RenderTexture.active = currentActiveRT;
+            return texture2D;
+        }
+
+        internal static RenderTexture toRenderTexture(Texture2D texture2D)
+        {
+            RenderTexture renderTexture = new RenderTexture();
+            RenderTexture currentActiveRT = RenderTexture.active; //not sure if i actually need this
+
+            Graphics.Blit(texture2D, renderTexture);
+
+            RenderTexture.active = currentActiveRT;
+            return renderTexture;
         }
 
         internal static void SaveTexture(RenderTexture renderTexture, string nameOfPNG, int resolution)
