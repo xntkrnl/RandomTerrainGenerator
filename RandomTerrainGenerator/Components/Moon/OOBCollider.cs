987902f1d9be1cf805c4c5fe4822eaedf1ddefae
Nikita <zhuravlyov.neke@yandex.ru>
2025-03-18 06:34:40 +0300

    OOBCollider script

---
+++ RandomTerrainGenerator/Components/Moon/OOBCollider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace RandomTerrainGenerator.Components.Moon
+{
+    [RequireComponent(typeof(Collider))]
+    internal class OOBCollider : MonoBehaviour
+    {
+        [SerializeField] private int targetLayer; 
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == targetLayer)
+                other.transform.forward *= -1;
+            //not tested
+        }
+    }
+}
